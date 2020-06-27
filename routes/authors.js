289d85ae2f94aa2c1112fd73c06da2d3cc8f9daf
Adel <abdelrhman66166@gmail.com>
2020-06-27 06:22:34 +0200

    edited the creat author route to save the name in new Aurthor object and added errorMessage.ejs partial view

--- routes/authors.js
+++ routes/authors.js
@@ -15,7 +15,19 @@ router.get('/new', (req, res) => {
 
 // Creat Author Route
 router.post('/', (req, res) => {
-    res.send(req.body.name)
+    const author = new Author({
+        name: req.body.name
+    })
+    author.save((err, newAuthor) => {
+        if (err) {
+            res.render('authors/new', {
+                author: author,
+                errorMessage: 'Error creating Author'
+            })
+        } else {
+            res.redirect('authors')
+        }
+    })
 })
 
 module.exports= router
