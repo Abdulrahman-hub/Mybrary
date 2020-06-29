097103e7154741531e1ee9f70f19471f5884dc42
Adel <abdelrhman66166@gmail.com>
2020-06-29 18:18:03 +0200

    created the books route

---
+++ routes/books.js
@@ -0,0 +1,21 @@
+const express = require('express')
+const router = express.Router()
+const Book = require('../models/book')
+
+// All books Route
+router.get('/', async (req, res) => {
+    
+    
+})
+
+// New book Route
+router.get('/new', (req, res) => {
+    
+})
+
+// Creat book Route
+router.post('/', async (req, res) => {
+    
+})
+
+module.exports = router
