6c17ffc6226eb8d5dab4188d7d0dce917885bd87
Adel <abdelrhman66166@gmail.com>
2020-06-29 20:09:06 +0200

    created new book form

--- routes/books.js
+++ routes/books.js
@@ -1,21 +1,37 @@
 const express = require('express')
 const router = express.Router()
 const Book = require('../models/book')
+const Author = require('../models/author')
+
+
 
 // All books Route
 router.get('/', async (req, res) => {
-    
-    
+    res.send('All Books')
+
 })
 
 // New book Route
-router.get('/new', (req, res) => {
-    
+router.get('/new', async (req, res) => {
+    try {
+        const authors = await Author.find({})
+        const book = new Book
+        res.render('books/new', {
+            authors: authors,
+            book: book
+        })
+    } catch (err) {
+        res.redirect('/')
+        console.log(err);
+        
+    }
+
 })
 
 // Creat book Route
 router.post('/', async (req, res) => {
-    
+    res.send('Create Books')
+
 })
 
 module.exports = router
