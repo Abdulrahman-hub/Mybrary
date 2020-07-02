ae1e34b968737edcb11d98ca69eaed9b80dd50a1
Adel <abdelrhman66166@gmail.com>
2020-07-02 02:32:40 +0200

    file upload database

---
+++ public/javascripts/fileUpload.js
@@ -0,0 +1,18 @@
+
+FilePond.registerPlugin(
+    FilePondPluginImagePreview,
+    FilePondPluginImageResize,
+    FilePondPluginFileEncode
+)
+
+FilePond.setOptions({
+    stylePanelAspectRatio: 150 / 100,
+    imageResizeTargetWidth: 100,
+    imageResizeTargetHeight: 150
+})
+const inputElement = document.querySelector('input[type="file"]');
+
+const pond = FilePond.create( inputElement );
+
+
+
